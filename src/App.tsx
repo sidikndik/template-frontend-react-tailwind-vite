b4006d8939449330be02fbcd0a88156300116541
sidikndik <sidikndik92@gmail.com>
2026-01-11 11:09:45 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="flex h-screen w-full items-center justify-center bg-slate-900">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-sky-400 underline">
+          Hello Tailwind + TypeScript!
+        </h1>
+        <p className="mt-4 text-slate-300">
+          If you see this styled, your setup is complete.
+        </p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
